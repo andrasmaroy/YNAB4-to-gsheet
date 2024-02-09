830a3f7691d163f78c3c30034b4fb1a266cbadc9
András Maróy <andras@maroy.hu>
2024-02-09 14:26:19 +0100

    Implement storing monthly budgets

--- gsheet.py
+++ gsheet.py
@@ -1,5 +1,7 @@
 import gspread
 
+from datetime import datetime
+
 
 def create_sheets(spreadsheet: gspread.Spreadsheet, extra_txn_curs: list[str]):
     ynab_sheets = [
@@ -82,9 +84,64 @@ def store_categories(data, worksheet: gspread.worksheet.Worksheet):
             end_col = -1
 
 
-def store_transactions(data, worksheet: gspread.worksheet.Worksheet):
-    pass
-
-
 def store_budgets(data, worksheet: gspread.worksheet.Worksheet):
+    budgets = [["", ""], ["", ""]]
+
+    first_transaction_date = datetime.strptime(
+        data.get("transactions")[0].get("date"), "%Y-%m-%d"
+    )
+    for budget in data.get("monthlyBudgets"):
+        # Nothing budgeted for the month, skip
+        if len(budget.get("monthlySubCategoryBudgets")) == 0:
+            continue
+
+        # Only deleted budgets for the month, skip
+        if (
+            len(
+                list(
+                    filter(
+                        lambda mscb: not mscb.get("isTombstone", False)
+                        and mscb.get("budgeted", 0) > 0,
+                        budget.get("monthlySubCategoryBudgets"),
+                    )
+                )
+            )
+            == 0
+        ):
+            continue
+
+        # Budget from before starting the budget, skip
+        if datetime.strptime(budget.get("month"), "%Y-%m-%d") < first_transaction_date:
+            continue
+
+        budgets[0].append(budget.get("entityId"))
+        budgets[1].append(budget.get("month"))
+        for subcategory_budget in budget.get("monthlySubCategoryBudgets"):
+            if subcategory_budget.get("isTombstone", False):
+                continue
+            row_to_insert = -1
+            for i, row in enumerate(budgets):
+                if row[0] == subcategory_budget.get("categoryId"):
+                    row_to_insert = i
+                    break
+            if row_to_insert == -1:
+                budgets.append([subcategory_budget.get("categoryId"), ""])
+                row_to_insert = len(budgets) - 1
+
+            column_to_insert = len(budgets[0]) - 1
+            if len(budgets[row_to_insert]) < column_to_insert:
+                spaces_needed = column_to_insert - len(budgets[row_to_insert])
+                budgets[row_to_insert].extend([0 for _ in range(spaces_needed)])
+            budgets[row_to_insert].append(subcategory_budget.get("budgeted"))
+    budgets[2][1] = "=ARRAYFORMULA(HLOOKUP(A3:A;'YNAB/Categories'!A$3:ZZ$4;2;false))"
+
+    worksheet.clear()
+    worksheet.resize(len(budgets), len(budgets[0]))
+    worksheet.update(budgets, raw=False)
+    worksheet.freeze(2, 2)
+    worksheet.hide_rows(0, 1)
+    worksheet.hide_columns(0, 1)
+
+
+def store_transactions(data, worksheet: gspread.worksheet.Worksheet):
     pass
--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 from config import init_config, get_config
 from dbx import find_latest_yfull
 from dropbox import Dropbox
-from gsheet import create_sheets, store_categories
+from gsheet import create_sheets, store_budgets, store_categories
 from gspread import oauth
 
 
@@ -17,3 +17,4 @@ if __name__ == "__main__":
     spreadsheet = gc.open(config["GSPREAD_SHEET_NAME"])
     create_sheets(spreadsheet, list(config["BUDGET_EXTRA_TXN"].keys()))
     store_categories(main_budget_data, spreadsheet.worksheet("YNAB/Categories"))
+    store_budgets(main_budget_data, spreadsheet.worksheet("YNAB/Budgets"))
