53d45f34e0c350482d19603b8c6431804c00d2d7
András Maróy <andras@maroy.hu>
2024-02-09 11:58:58 +0100

    Setup dropbox client

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
 from config import init_config, get_config
+from dropbox import Dropbox
 
 
 if __name__ == "__main__":
     init_config()
     config = get_config()
+    dbx = Dropbox(config["DROPBOX_ACCESS_TOKEN"])
