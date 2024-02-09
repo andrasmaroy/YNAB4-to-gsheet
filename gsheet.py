1afc2b4af2c7661b4f15b87739b7b3e7c17a0a92
András Maróy <andras@maroy.hu>
2024-02-09 14:00:38 +0100

    Implement creating necessary sheets

--- gsheet.py
+++ gsheet.py
@@ -2,7 +2,24 @@ import gspread
 
 
 def create_sheets(spreadsheet: gspread.Spreadsheet, extra_txn_curs: list[str]):
-    pass
+    ynab_sheets = [
+        {"name": "Categories", "rows": 4, "cols": 1},
+        {"name": "Budgets", "rows": 2, "cols": 2},
+        {"name": "Transactions", "rows": 1, "cols": 7},
+    ]
+    for cur in extra_txn_curs:
+        ynab_sheets.append({"name": "Transactions{}".format(cur), "rows": 1, "cols": 7})
+
+    for sheet in ynab_sheets:
+        try:
+            # Try to get the worksheet; if it doesn't exist, create it
+            spreadsheet.worksheet("YNAB/{}".format(sheet["name"]))
+        except gspread.exceptions.WorksheetNotFound:
+            spreadsheet.add_worksheet(
+                title="YNAB/{}".format(sheet["name"]),
+                rows=sheet["rows"],
+                cols=sheet["cols"],
+            )
 
 
 def store_categories(data, worksheet: gspread.worksheet.Worksheet):
--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 from config import init_config, get_config
 from dbx import find_latest_yfull
 from dropbox import Dropbox
+from gsheet import create_sheets
 from gspread import oauth
 
 
@@ -10,7 +11,8 @@ if __name__ == "__main__":
     dbx = Dropbox(config["DROPBOX_ACCESS_TOKEN"])
     gc = oauth(
         credentials_filename=config["GSPREAD_CREDENTIALS_FILENAME"],
-        authorized_user_filename=config["GSPREAD_AUTHORIZED_USER_FILENAME"]
+        authorized_user_filename=config["GSPREAD_AUTHORIZED_USER_FILENAME"],
     )
     main_budget_data = find_latest_yfull(dbx, config["BUDGET"])
     spreadsheet = gc.open(config["GSPREAD_SHEET_NAME"])
+    create_sheets(spreadsheet, list(config["BUDGET_EXTRA_TXN"].keys()))
