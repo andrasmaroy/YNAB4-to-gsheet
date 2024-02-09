4d3320ed5bcb54054451f13a735a57661847f215
András Maróy <andras@maroy.hu>
2024-02-09 14:20:43 +0100

    Implement storing ynab categories

--- gsheet.py
+++ gsheet.py
@@ -23,7 +23,63 @@ def create_sheets(spreadsheet: gspread.Spreadsheet, extra_txn_curs: list[str]):
 
 
 def store_categories(data, worksheet: gspread.worksheet.Worksheet):
-    pass
+    categories = [[], [], [], []]
+    for master_category in data.get("masterCategories", []):
+        # Empty master category, skip
+        if (
+            master_category.get("subCategories") is None
+            or len(master_category.get("subCategories")) == 0
+        ):
+            continue
+
+        # Deleted master category, skip
+        if master_category.get("isTombstone", False):
+            continue
+
+        # All subcategories deleted, skip
+        subcategories = list(
+            filter(
+                lambda sc: not sc.get("isTombstone", False),
+                master_category.get("subCategories"),
+            )
+        )
+        if len(subcategories) == 0:
+            continue
+
+        categories[0].append(master_category.get("entityId"))
+        categories[1].append(master_category.get("name"))
+        for i, subcategory in enumerate(subcategories):
+            if i > 0:
+                categories[0].append(master_category.get("entityId"))
+                categories[1].append("")
+            categories[2].append(subcategory.get("entityId"))
+            categories[3].append(subcategory.get("name"))
+
+    worksheet.clear()
+    worksheet.unmerge_cells("2:2")
+    worksheet.resize(4, len(categories[0]))
+    worksheet.update(categories)
+    worksheet.hide_rows(0, 1)
+    worksheet.hide_rows(2, 3)
+
+    # Merge master category cells over their subcategories
+    start_col = -1
+    end_col = -1
+    for i, master_category in enumerate(categories[1]):
+        if master_category != "":
+            if start_col == -1:
+                start_col = i
+            else:
+                end_col = i
+        if master_category == "":
+            continue
+        if start_col != -1 and end_col != -1:
+            start_cell = gspread.utils.rowcol_to_a1(2, start_col + 1)
+            end_cell = gspread.utils.rowcol_to_a1(2, end_col)
+            worksheet.merge_cells("{}:{}".format(start_cell, end_cell))
+
+            start_col = i
+            end_col = -1
 
 
 def store_transactions(data, worksheet: gspread.worksheet.Worksheet):
--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 from config import init_config, get_config
 from dbx import find_latest_yfull
 from dropbox import Dropbox
-from gsheet import create_sheets
+from gsheet import create_sheets, store_categories
 from gspread import oauth
 
 
@@ -16,3 +16,4 @@ if __name__ == "__main__":
     main_budget_data = find_latest_yfull(dbx, config["BUDGET"])
     spreadsheet = gc.open(config["GSPREAD_SHEET_NAME"])
     create_sheets(spreadsheet, list(config["BUDGET_EXTRA_TXN"].keys()))
+    store_categories(main_budget_data, spreadsheet.worksheet("YNAB/Categories"))
