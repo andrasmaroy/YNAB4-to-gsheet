4a746e3c7590e0211f092f796a4f53bd29589a7c
András Maróy <andras@maroy.hu>
2024-03-01 11:18:57 +0100

    Switch Dropbox client to OAuth2

--- config.py
+++ config.py
@@ -7,7 +7,8 @@ def init_config():
         separator="__",
         whitelist=[
             "BUDGET",
-            "DROPBOX_ACCESS_TOKEN",
+            "DROPBOX_APP_KEY",
+            "DROPBOX_APP_SECRET",
             "GSPREAD_AUTHORIZED_USER_FILENAME",
             "GSPREAD_CREDENTIALS_FILENAME",
             "GSPREAD_SHEET_NAME",
@@ -17,6 +18,7 @@ def init_config():
 
     Pconf.defaults(
         {
+            "DROPBOX_OAUTH_TOKEN_FILENAME": "/run/secrets/token-dropbox.json",
             "GSPREAD_AUTHORIZED_USER_FILENAME": "/run/secrets/token.json",
             "GSPREAD_CREDENTIALS_FILENAME": "/run/secrets/credentials.json",
             "LOG_LEVEL": "INFO",
--- main.py
+++ main.py
@@ -1,8 +1,11 @@
+import json
 import logging
 
 from config import init_config, get_config
+from datetime import datetime
 from dbx import find_latest_yfull
 from dropbox import Dropbox
+from dropbox.oauth import OAuth2FlowNoRedirectResult
 from gsheet import create_sheets, store_budgets, store_categories, store_transactions
 from gspread import oauth
 
@@ -13,7 +16,24 @@ if __name__ == "__main__":
     logging.basicConfig(level=config["LOG_LEVEL"].upper())
 
     logging.info("Initializing Dropbox")
-    dbx = Dropbox(config["DROPBOX_ACCESS_TOKEN"])
+    with open(config["DROPBOX_OAUTH_TOKEN_FILENAME"], "r") as fp:
+        token = json.load(fp)
+        dbx_oauth_token = OAuth2FlowNoRedirectResult(
+            token["access_token"],
+            token["account_id"],
+            token["user_id"],
+            token["refresh_token"],
+            datetime.fromisoformat(token["expires_at"]),
+            token["scope"],
+        )
+        dbx = Dropbox(
+            oauth2_access_token=dbx_oauth_token.access_token,
+            oauth2_refresh_token=dbx_oauth_token.refresh_token,
+            oauth2_access_token_expiration=dbx_oauth_token.expires_at,
+            app_key=config["DROPBOX_APP_KEY"],
+            app_secret=config["DROPBOX_APP_SECRET"],
+            scope=dbx_oauth_token.scope.split(),
+        )
 
     logging.info("Initializing Google")
     gc = oauth(
